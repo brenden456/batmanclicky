c9c807ff1503709f408df3641f3dfe0b645518a7
brenden456 <brendenkemp10@icloud.com>
2025-07-05 11:20:50 -0500

    batman button baterang

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 let clicks;
 let clickrate = 1
-let clickprice = 20
+let clickprice = 30
 let clickupgrades = 1
-document.getElementById("upgrade1"). innerHTML = clickprice + "+ per click batterang all points";
+document.getElementById("upgrade1"). innerHTML = clickprice + "  batterang 2x per click ";
 function clicker() {
   clicks = Number(document.getElementById("counter").innerHTML)
   clicks = clicks + clickrate
@@ -24,10 +24,10 @@ function upgrade() {
 //   clicks = clicks -20
 console.log("i was clicked")
 clicks = clicks - clickprice;
-clickrate = clickrate + (clickprice * clickupgrades);
+clickrate = clickrate + (2 * clickupgrades);
 clickupgrades++;
-clickprice = clickprice * clickupgrades;
-document.getElementById("upgrade1"). innerHTML = clickprice + "+ per click batterang all points"
+clickprice = 30 * clickupgrades;
+document.getElementById("upgrade1"). innerHTML = clickprice + " batterang 2x per click"
 document.getElementById("counter").innerHTML = clicks
 checkupgrade()
 }
