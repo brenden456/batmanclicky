3054f9b9b3beceded9b6a800d20c9256e72ab78b
brenden456 <brendenkemp10@icloud.com>
2025-07-26 11:56:37 -0500

    boing boing batman

--- src/main.js
+++ src/main.js
@@ -3,19 +3,23 @@ let clickrate = 1
 let clickprice = 30
 let clickupgrades = 1
 document.getElementById("upgrade1"). innerHTML = clickprice + "  batterang 2x per click ";
+
 function clicker() {
   clicks = Number(document.getElementById("counter").innerHTML)
   clicks = clicks + clickrate
   document.getElementById("counter").innerHTML = clicks
+  console.log("click price: $", clickprice);
   checkupgrade()
 }
 
 function checkupgrade() {
-/* console.log(clicks>=20, clicks); */
-  if (Number(document.getElementById("counter").innerHTML) >= clickprice) { 
-    document.getElementById("upgrade1").classList.remove("ishidden")
+  console.log(clicks>=clickprice, clicks);
+  console.log(document.getElementById("counter").innerHTML >= clickprice);
+  if (document.getElementById("counter").innerHTML >= clickprice) { 
+    console.log("remove ishidden");
+    document.getElementById("upgrade1").classList.remove("isHidden");
   } else {
-    document.getElementById("upgrade1").classList.add("ishidden")
+    document.getElementById("upgrade1").classList.add("isHidden")
   }
 }
 
